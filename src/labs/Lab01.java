9630f1313a62f9382fe4110028fd57b216804693
cloudyskyes06 <shart@newtech.coppellisd.com>
2022-02-07 16:06:53 -0600

    first few problems

--- src/labs/Lab01.java
+++ src/labs/Lab01.java
@@ -1,5 +1,5 @@
 package labs;
-
+import java.util.Scanner;
 
 
 public class Lab01 {
@@ -17,8 +17,15 @@ public class Lab01 {
 	
 	public static void problem01() {
 		
+		Scanner inKey = new Scanner(System.in);
+		System.out.println("Enter a positive integer: ");
+		int input = inKey.nextInt();
+		Scanner inKey2 = new Scanner(System.in);
+		System.out.println("Enter another positive integer: ");
+		int input2 = inKey2.nextInt();
 		
-		
+		double num1 = Math.pow(input, input2);
+		System.out.println(input + "^" + input2 + " = " + num1);
 	}
 	
 	
@@ -26,8 +33,16 @@ public class Lab01 {
 	
 	public static void problem02() {
 		
+		System.out.println("\n\n");
+		Scanner inKey3 = new Scanner(System.in);
+		System.out.println("Enter a positive integer: ");
+		int input3 = inKey3.nextInt();
 		
+		double num2 = Math.sqrt(input3);
 		
+		System.out.println("The square root of " + input3 + " is " + num2);
+		
+		System.out.println("\n\n\n");
 	}
 
 	
@@ -35,7 +50,16 @@ public class Lab01 {
 
 	public static void problem03() {
 		
+		System.out.println("Enter the sides of the right triangle.");
+		Scanner inKey4 = new Scanner(System.in);
+		System.out.println("Length of side A: ");
+		int sideA = inKey4.nextInt();
 		
+		Scanner inKey5 = new Scanner(System.in);
+		System.out.println("Length of side B: ");
+		int sideB = inKey5.nextInt();
+		
+		//A^2 + B^2 = C^2
 		
 
 	}
